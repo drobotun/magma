83799295ae42246a6af6e1821f2795ff095d57a2
Evgeny Drobotun <evgeny@drobotun.su>
2026-05-10 19:58:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
     magma_ctx ctx;
-    uint8_t out_blk[8];
+    vect block;
     magma_init(&ctx, test_key);
-    magma_encrypt(&ctx, encrypt_test_string, out_blk);
-    magma_decrypt(&ctx, decrypt_test_string, out_blk);
+    magma_encrypt(&ctx, encrypt_test_string, block);
+    magma_decrypt(&ctx, decrypt_test_string, block);
     return 0;
 }
